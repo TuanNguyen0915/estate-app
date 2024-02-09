d42da834596144c169f8d51edb103935c6a6b52d
Tuan Nguyen <tuan.nat915@gmail.com>
2024-02-09 16:34:23 -0500

    create all pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
 
 function App() {
   return (
-    <div className="text-center text-3xl text-red-500 hover:text-blue-500 hover:underline">
-      App
+    <div className="mx-auto w-full max-w-[1440px] p-4">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }

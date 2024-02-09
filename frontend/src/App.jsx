e1525bb175053ce7499b5ac96341ffbb6d86a423
Tuan Nguyen <tuan.nat915@gmail.com>
2024-02-09 16:26:42 -0500

    init app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-center text-3xl text-red-500 hover:text-blue-500 hover:underline">
+      App
+    </div>
+  );
+}
+
+export default App;
